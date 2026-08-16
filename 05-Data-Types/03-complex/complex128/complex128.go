7bb198393872a58c2aaca4e180323ea71f7f7574
Krish Mishra <krishmishra4444@gmail.com>
2026-08-16 10:41:48 +0530

    practiced complex numbers

---
+++ 05-Data-Types/03-complex/complex128/complex128.go
@@ -0,0 +1,13 @@
+// example of complex128 in golang
+
+package main
+
+import "fmt"
+
+func main() {
+	var number complex128 = 5 + 6i
+
+	fmt.Println(number)
+}
+
+// prints the complex number which is 5 + 6i
