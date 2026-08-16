6076cdc1cfca4c0a915ddf938da99c4c43373f54
Krish Mishra <krishmishra4444@gmail.com>
2026-08-16 10:33:12 +0530

    practiced float

---
+++ 05-Data-Types/02-float/float32/float32.go
@@ -0,0 +1,14 @@
+// example of float32 in golang
+
+
+package main
+
+import "fmt"
+
+func main() {
+	var price float32 = 99.99
+
+	fmt.Println(price)
+}
+
+// prints price which is 99.99
---
+++ 05-Data-Types/02-float/float64/float64.go
@@ -0,0 +1,13 @@
+// example of float64 in golang
+
+package main
+
+import "fmt"
+
+func main() {
+	var price float64 = 999.99
+
+	fmt.Println(price)
+}
+
+// prints price which is 999.99 
