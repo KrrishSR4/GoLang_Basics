37af2801861005b36cb16f8a9eda278189a19b0d
Krish Mishra <krishmishra4444@gmail.com>
2026-08-16 10:45:28 +0530

    practiced complex numbers

---
+++ 05-Data-Types/03-complex/complex64/complex64.go
@@ -0,0 +1,13 @@
+// example of complex64 in golang
+
+package main
+
+import "fmt"
+
+func main() {
+	var number complex64 = 2 + 3i
+
+	fmt.Println(number)
+}
+
+// prints the complex number which is 2 + 3i
