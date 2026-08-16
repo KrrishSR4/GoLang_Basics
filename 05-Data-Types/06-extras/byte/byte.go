6ded6618822ec71179f130e34e25ac71415a494d
Krish Mishra <krishmishra4444@gmail.com>
2026-08-16 11:07:21 +0530

    practiced byte unicode

---
+++ 05-Data-Types/06-extras/byte/byte.go
@@ -0,0 +1,12 @@
+// example of byte in golang
+ package main 
+
+ import "fmt"
+
+ func main () {
+	var character byte = 'A'
+
+	fmt.Println(character)
+ }
+
+ // prints 65 because, A has Unicode code point 65.
