2a8be8c3d06e43608d4467d42110ff6fdb072eb0
Krish Mishra <krishmishra4444@gmail.com>
2026-08-16 11:07:49 +0530

    practiced rune unicode

---
+++ 05-Data-Types/06-extras/rune/rune.go
@@ -0,0 +1,12 @@
+// example of rune in golang
+ package main 
+
+ import "fmt"
+
+ func main () {
+	var character rune = 'A'
+
+	fmt.Println(character)
+ }
+
+ // prints 65 because, A has Unicode code point 65.
