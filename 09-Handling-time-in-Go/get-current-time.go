7289f21be051909ced5429a2e738147333ca6923
Krish Mishra <krishmishra4444@gmail.com>
2026-08-30 17:15:28 +0530

    practiced handling time in golang

---
+++ 09-Handling-time-in-Go/get-current-time.go
@@ -0,0 +1,16 @@
+// Example of getting current time in golang
+
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main  () {
+
+	now := time.Now() 
+
+	fmt.Println(now)
+}
---
+++ 09-Handling-time-in-Go/get-individual-values.go
@@ -0,0 +1,23 @@
+// Example of getting individual values in golang
+
+package main 
+
+import (
+	"fmt"
+	"time"
+)
+
+
+func main () {
+
+	now := time.Now()
+
+	fmt.Println("Year: ", now.Year())
+	fmt.Println("Month: ", now.Month())
+	fmt.Println("Day: ", now.Day())
+	fmt.Println("Hour: ", now.Hour())
+	fmt.Println("Minute: ", now.Minute())
+	fmt.Println("Second: ", now.Second())
+	fmt.Println("Nanosecond: ", now.Nanosecond())
+	fmt.Println("Weekday: ", now.Weekday())
+} 
