33b0f5c4add0412a22caf40fa4387c68310ac7b5
Krish Mishra <krishmishra4444@gmail.com>
2026-08-31 21:30:43 +0530

    practiced arithmetic operations

---
+++ 12-operators/arithmetic.go
@@ -0,0 +1,17 @@
+// example of arithmetic operator in golang
+
+package main 
+
+import "fmt"
+
+func main () {
+	a := 10
+	b := 4
+
+	fmt.Println(a + b)
+	fmt.Println(a - b)
+	fmt.Println(a * b)
+	fmt.Println(a / b)
+	fmt.Println(a % b)
+}
+
