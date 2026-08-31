15907511b3be472c8fcee8a717aeed5e34182aad
Krish Mishra <krishmishra4444@gmail.com>
2026-09-01 02:44:47 +0530

    practiced ifelse in golang

---
+++ 14-control-flow/if-else.go
@@ -0,0 +1,16 @@
+// Example of if - else in golang
+
+package main
+
+import "fmt"
+
+func main () {
+
+	age := 12
+
+	if age >= 18 {
+		fmt.Println("adult")
+	} else {
+		fmt.Println("minor")
+	}
+}
