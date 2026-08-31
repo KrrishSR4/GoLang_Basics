f41fc5aeb53b24c8cbe01c050c15e08e9fdeb04b
Krish Mishra <krishmishra4444@gmail.com>
2026-09-01 03:03:22 +0530

    practiced all control flow in golang

---
+++ 14-control-flow/continue.go
@@ -0,0 +1,15 @@
+// Example of continue control flow in golang
+
+package main
+
+import "fmt"
+
+func main () {
+
+	for i := 1; i <= 5; i++ {
+		if i == 3 {
+			continue
+		}
+		fmt.Println(i)
+	}
+}
