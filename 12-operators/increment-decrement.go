cf313bdedcebd6c05e149c2cb88279a176c2b89f
Krish Mishra <krishmishra4444@gmail.com>
2026-08-31 21:50:19 +0530

    practiced increment and decrement operators in golang

---
+++ 12-operators/increment-decrement.go
@@ -0,0 +1,17 @@
+// Example of increment and decrement operators in golang
+
+package main 
+
+import "fmt"
+
+func main () {
+
+	a := 10
+	b := 20
+
+	a++
+	fmt.Println(a)
+
+	b--
+	fmt.Println(b)
+}
