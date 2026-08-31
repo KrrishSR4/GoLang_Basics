571b898770b51f6d533e4ec68ac567ff9846b966
Krish Mishra <krishmishra4444@gmail.com>
2026-09-01 02:12:12 +0530

    practiced printf() in golang

---
+++ 13-Input-Output/fmt-Printf().go
@@ -0,0 +1,15 @@
+// example of Printf() in golang
+
+package main 
+
+import "fmt"
+
+func main () {
+
+	name := "Krish"
+	age := 21
+
+	fmt.Println("Name: %s\n", name)
+	fmt.Println("age: %d\n", age)
+
+}
