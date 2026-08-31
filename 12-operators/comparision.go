ee2cfe11b4b1a654a1a5d67c33f9cf19fb369442
Krish Mishra <krishmishra4444@gmail.com>
2026-08-31 21:39:35 +0530

    practiced comparision operator

---
+++ 12-operators/comparision.go
@@ -0,0 +1,18 @@
+// Example of comparision operator in golang
+
+package main 
+
+import "fmt"
+
+func main () {
+
+	a := 10
+	b := 20
+
+	fmt.Println(a == b)
+	fmt.Println(a != b)
+	fmt.Println(a >  b)
+	fmt.Println(a <  b)
+	fmt.Println(a >= b)
+	fmt.Println(a <= b)
+}
