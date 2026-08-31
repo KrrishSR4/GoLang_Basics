6d4b1b3f6d263f6985b889545499f84d8baccf47
Krish Mishra <krishmishra4444@gmail.com>
2026-08-31 21:55:09 +0530

    practiced bitwise operator in golang

---
+++ 12-operators/bitwise.go
@@ -0,0 +1,17 @@
+// example of bitwise operator in golang
+
+package main
+
+import "fmt"
+
+func main () {
+
+	a := 5
+	b := 3
+
+	fmt.Println(a & b)
+	fmt.Println(a | b)
+	fmt.Println(a ^ b)
+	fmt.Println(a << 1)
+	fmt.Println(a >> 1)
+}
