911dc7e7cb7e494abdcb4a875b53bcbc9cd8c9bb
Krish Mishra <krishmishra4444@gmail.com>
2026-09-01 02:15:41 +0530

    practiced scan() in golang

---
+++ 13-Input-Output/fmt.scan().go
@@ -0,0 +1,20 @@
+// Example of scan in golang
+
+package main 
+
+import "fmt"
+
+func main () {
+
+	var name string
+	var age int
+
+	fmt.Print("Enter your name: ")
+	fmt.Scan(&name)
+
+	fmt.Print("Enter your age: ")
+	fmt.Scan(&age)
+
+	fmt.Println("Name :", name)
+	fmt.Println("Age :", age)
+}
