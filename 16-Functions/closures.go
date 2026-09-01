f7f258551718476e401dbc4b5d1e42be8d4b0f36
Krish Mishra <krishmishra4444@gmail.com>
2026-09-02 01:23:42 +0530

    practiced closures functions in golang

---
+++ 16-Functions/closures.go
@@ -0,0 +1,23 @@
+// Example of closures function in golang
+
+
+package main
+
+import "fmt"
+
+func counter() func() int {
+	count := 0
+
+	return func() int {
+		count++
+		return count
+	}
+}
+
+func main() {
+	next := counter()
+
+	fmt.Println(next())
+	fmt.Println(next())
+	fmt.Println(next())
+}
