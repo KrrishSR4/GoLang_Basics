38a790f70608c9a07387d81d6e1fb8dd4a07df91
Krish Mishra <krishmishra4444@gmail.com>
2026-09-01 20:59:59 +0530

    practied return values in functions in golang

---
+++ 16-Functions/return.go
@@ -0,0 +1,15 @@
+// Example of return values in functions in golang
+
+package main 
+
+import "fmt"
+
+func add (a int, b int) int {
+
+	return a + b
+}
+
+func main () {
+	result := add(10, 20)
+	fmt.Println(result)
+}
