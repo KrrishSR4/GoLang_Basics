02909879d99bccfda36b36883ce27d48d226e477
Krish Mishra <krishmishra4444@gmail.com>
2026-09-01 18:06:49 +0530

    practiced all loops in golang

---
+++ 15-Loops/loop-break.go
@@ -0,0 +1,16 @@
+// Example of doing break in loop in golang
+
+package main
+
+import "fmt"
+
+func main () {
+
+	for i := 1; i <= 5; i++ {
+		
+		if i ==3 {
+			break
+		}
+		fmt.Println(i)
+	}
+}
