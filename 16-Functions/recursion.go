570bc73950f2da6c0e0bf8dab85310835aebe88c
Krish Mishra <krishmishra4444@gmail.com>
2026-09-02 01:30:43 +0530

    practiced functions and its various types

---
+++ 16-Functions/recursion.go
@@ -0,0 +1,22 @@
+// Example of recursion in function in golang
+
+
+package main
+
+import "fmt"
+
+func factorial (n int) int {
+
+	if n == 1 {
+		return 1
+	}
+
+	return n * factorial (n - 1)
+}
+
+func main () {
+
+	result := factorial (5)
+
+	fmt.Println(result)
+}
