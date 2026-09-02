e083d972f81d3a73e035604c9b81887ce6d5169e
Krish Mishra <krishmishra4444@gmail.com>
2026-09-02 21:24:50 +0530

    practiced array declaration in golang

---
+++ 17-arrays/array-declaration.go
@@ -0,0 +1,15 @@
+// Example of declaring an array in golang
+
+
+package main
+
+import "fmt"
+
+func main () {
+
+	var numbers [5] int
+
+	fmt.Println(numbers)
+}
+
+// prints [0 0 0 0 0] because of no value.
